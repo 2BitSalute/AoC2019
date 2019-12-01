193e0d90a482c8d4a68907c3c4b5d892a0337756
Tatiana Racheva <tatianaracheva@Tatianas-MacBook-Air.local>
2019-12-01 15:42:08 -0800

    Day 1 part 2

--- day01/main.ml
+++ day01/main.ml
@@ -1,7 +1,12 @@
-let required_fuel (x : string) : int =
-    Printf.printf "Module weight: %s " x;
-    -2 + int_of_float (floor ((float_of_int (int_of_string x)) /. 3.))
-
+let required_fuel (input : string) : int =
+    Printf.printf "Module weight: %s -- " input;
+    let rec required_fuel acc mass =
+        let fuel = -2 + int_of_float (floor ((float_of_int mass) /. 3.)) in
+        if fuel > 0 then required_fuel (acc + fuel) fuel
+        else acc
+    in
+    let mass = int_of_string input in
+    required_fuel 0 mass
 let () =
     Printf.printf "Yay";
     let chan = (open_in "./input") in
@@ -14,11 +19,10 @@ let () =
         with
         | End_of_file ->
             (* Fun fact: trying to close the channel here results in a silent crash *)
-            close_in chan;
             acc
         | _e -> Printf.printf "Some other error!"; acc
     in
     let total = read_input 0 in
     close_in chan;
-    Printf.printf "Total: %d\n%!" total;
+    Printf.printf "Total fuel required: %d\n%!" total;
     ()
